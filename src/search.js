97a68f8b3eb3648e98421274347582e30dd6e55e
sunxiaojie79 <297280911@qq.com>
2020-06-29 00:35:20 +0800

    feat:解析less

--- src/search.js
+++ src/search.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import './search.css'
+import './search.less'
 class Search extends React.Component {  
   render () { 
     return <div className="search-text">search text</div>
