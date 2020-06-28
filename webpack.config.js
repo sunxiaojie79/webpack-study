4f52311330592403e1dca8639a9784846ae7f912
sunxiaojie79 <297280911@qq.com>
2020-06-28 23:14:05 +0800

    feat:安装webpack

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  mode: 'production',
+}
