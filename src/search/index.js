6ce65add871da2aa5c6c4fd8106be46961d656ae
sunxiaojie79 <297280911@qq.com>
2020-07-02 01:54:46 +0800

    feat:动态import

--- src/search/index.js
+++ src/search/index.js
@@ -3,14 +3,33 @@ import ReactDom from 'react-dom';
 import './search.less'
 import logo from './images/webpack-logo.png'
 import '../../common'
-import { a } from './tree-shaking'
-class Search extends React.Component {  
+// import { a } from './tree-shaking'
+class Search extends React.Component {
+  constructor(p) {
+    super(...arguments);
+
+    this.state = {
+      Text: null
+    };
+  }
+  loadComponent = () => {
+    import('./text.js').then((Text) => {
+      this.setState({
+        Text: Text.default
+      });
+    });
+  }
   render () {
-    const funcA = a()
+    const { Text } = this.state;
+    // const funcA = a()
     return <div className="search-text">
-      search text{funcA}
-              <img src={logo}/>
-            </div>
+      {
+        Text ? <Text /> : null
+      }
+      search text
+      {/* {funcA} */}
+      <img src={logo} onClick={this.loadComponent.bind(this)} />
+    </div>
   }
 }
 
