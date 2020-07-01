28304dde8ed64e05d5b0920cf678e3f7e20d49b9
sunxiaojie79 <297280911@qq.com>
2020-07-01 23:24:18 +0800

    feat:splitChunks,HtmlWebpackExternalsPlugin

--- src/search/index.js
+++ src/search/index.js
@@ -2,10 +2,9 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import './search.less'
 import logo from './images/webpack-logo.png'
+import '../../common'
 class Search extends React.Component {  
   render () {
-    a = 1
-      
     return <div className="search-text">
       search text
               <img src={logo}/>
--- webpack.prod.js
+++ webpack.prod.js
@@ -4,13 +4,14 @@ const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackExternalsPlugin = require('html-webpack-externals-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 const setMPA = () => {
   const entry = {};
   const htmlWebpackPlugins = [];
   const entryFiles = glob.sync(path.join(__dirname, './src/*/index.js'));
-  Object.keys(entryFiles).map(key => { 
+  Object.keys(entryFiles).map(key => {
     const entryFile = entryFiles[key]
     const match = entryFile.match(/src\/(.*)\/index\.js/);
     const pageName = match && match[1];
@@ -119,7 +120,41 @@ module.exports = {
       assetNameRegExp: /\.css$/g,
       cssProcessor: require('cssnano')
     }),
-    new CleanWebpackPlugin()
+    new CleanWebpackPlugin(),
+    // new HtmlWebpackExternalsPlugin({
+    //   externals: [
+    //     {
+    //       module: 'react',
+    //       entry: 'https://cdn.bootcdn.net/ajax/libs/react/16.13.1/umd/react.production.min.js',
+    //       global: 'React',
+    //     },
+    //     {
+    //       module: 'react-dom',
+    //       entry: 'https://cdn.bootcdn.net/ajax/libs/react-dom/16.13.1/umd/react-dom.production.min.js',
+    //       global: 'ReactDOM',
+    //     },
+    //   ],
+    //   files: [
+    //     `search.html` // 这里要指定html 不然还是会多次注入
+    //   ]
+    // })
   ].concat(htmlWebpackPlugins),
-  devtool: 'inline-source-map'
+  devtool: 'inline-source-map',
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        reactBase: {
+          test: /(react|react-dom)/,
+          name: 'vendors',
+          chunks: 'all',
+        },
+        commons: {
+          minSize: 0,
+          name: 'commons',
+          chunks: 'all',
+          minChunks: 2
+        }
+      }
+    }
+  }
 }
