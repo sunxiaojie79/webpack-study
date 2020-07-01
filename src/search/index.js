ad1e54dbf25e462abd7026215bd7edeb6cc732b2
sunxiaojie79 <297280911@qq.com>
2020-07-02 02:28:08 +0800

    feat: eslint

--- src/search/index.js
+++ src/search/index.js
@@ -1,39 +1,43 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import './search.less'
-import logo from './images/webpack-logo.png'
-import '../../common'
+import './search.less';
+import logo from './images/webpack-logo.png';
+import '../../common';
 // import { a } from './tree-shaking'
 class Search extends React.Component {
-  constructor(p) {
+  constructor() {
     super(...arguments);
 
     this.state = {
-      Text: null
+      Text: null,
     };
   }
+
   loadComponent = () => {
     import('./text.js').then((Text) => {
       this.setState({
-        Text: Text.default
+        Text: Text.default,
       });
     });
   }
-  render () {
+
+  render() {
     const { Text } = this.state;
     // const funcA = a()
-    return <div className="search-text">
-      {
-        Text ? <Text /> : null
-      }
-      search text
-      {/* {funcA} */}
-      <img src={logo} onClick={this.loadComponent.bind(this)} />
-    </div>
+    return (
+      <div className="search-text">
+        {
+          Text ? <Text /> : null
+        }
+        search text
+        {/* {funcA} */}
+        <img src={logo} alt="123" onClick={this.loadComponent.bind(this)} />
+      </div>
+    );
   }
 }
 
 ReactDom.render(
   <Search />,
-  document.getElementById('root')
-)
+  document.getElementById('root'),
+);
--- webpack.prod.js
+++ webpack.prod.js
@@ -52,7 +52,7 @@ module.exports = {
     rules: [
       {
         test: /.js$/,
-        use: 'babel-loader',
+        use: ['babel-loader', 'eslint-loader']
       },
       {
         test: /.css$/,
